99b1c27028120a83dbeab2e3b942e6034e7bc1cf
Ito Tomoya <41101653+tomoi@users.noreply.github.com>
2021-02-24 14:05:19 +0900

    top page responsive support

--- core_sys/js/live-countdown.js
+++ core_sys/js/live-countdown.js
@@ -1,14 +1,15 @@
+window.onload = function(){console.log('%c面白いものはなにもないよ。あるのはエラーログだけ。', 'font-weight: bold; font-size: large; color: #843D36');}
 function dateCounter() {
  
     var timer = setInterval(function() {
     //現在の日時取得
     var nowDate = new Date();
     //カウントダウンしたい日を設定
-    var anyDate = new Date("2021/03/12 13:54:30");//2月中は目をつぶってください
+    var anyDate = new Date("2021/03/12 13:54:30");
     //var anyDate = new Date("2021/02/12 13:54:30");//デバッグ用
     //日数を計算
     //var daysBetween = Math.ceil((anyDate - nowDate)/(1000*60*60*24));
-    var daysBetween = anyDate.getDay() - nowDate.getDay();
+    var daysBetween = anyDate.getDay() - nowDate.getDay();//2月中は目をつぶってください
     var ms = (anyDate - nowDate);
     if (ms >= 0) {
         //時間を取得
@@ -20,7 +21,7 @@ function dateCounter() {
         var s = Math.round((ms - h * 3600000 - m * 60000) / 1000);
  
         //HTML上に出力
-        document.getElementById("live").innerHTML = daysBetween + "日と" +_h + "時間" + m + "分" +s + "秒";
+        document.getElementById("live").innerHTML = "Live開始まであと" + daysBetween + "日と" +_h + "時間" + m + "分" +s + "秒";
  
         if ((h == 0) && (m == 0) && (s == 0)) {
         clearInterval(timer);
